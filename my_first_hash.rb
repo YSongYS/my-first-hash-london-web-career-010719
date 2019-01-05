b9f20f9be1f0d0b366a24b7c3fa3aa4f295b5c49
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-05 10:19:53 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_first_hash = {"name"=>"Songyishu", "age"=>"newly 27"}
   
 end
 
